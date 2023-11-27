a025c5a461b6e669ee024445e986d6438a8efc07
Tonnamiacart <tonnamiacart@gmail.com>
2023-11-27 15:32:12 -0800

    Task 7

---
+++ 0x00-python-hello_world/7-edges.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+word = "Holberton"
+word1 = "ola"
+word2 = "jide"
+word3 = "Tayo"
+print(f"First 3 letters: {word[:3]}")
+print(f"Last 2 letters: {word[7:]}")
+print(f"Middle word: {word[1:8]}")
