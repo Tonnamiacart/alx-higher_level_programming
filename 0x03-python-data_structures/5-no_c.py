a165c95693a53e2917cf18829831cfa047ceaf53
Tonnamiacart <tonnamiacart@gmail.com>
2023-12-03 15:13:31 -0800

    Task 5

--- 0x03-python-data_structures/5-no_c.py
+++ 0x03-python-data_structures/5-no_c.py
@@ -2,6 +2,6 @@
 def no_c(my_string):
    str1 = ""
    for char in my_string:
-       if char != 'c' and char != 'C':
-           str1 = str1 + char
+       if char != "c" and char != "C":
+           str =+ char
         return str1
